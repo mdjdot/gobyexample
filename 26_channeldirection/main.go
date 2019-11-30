8dab21a86b385d4f5ff9629aadada6f6e995f808
mengdejian <mengdejian@windimg.com>
2019-11-30 23:29:03 +0800

    channeldirection

---
+++ 26_channeldirection/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+	// pongs <- pings // channel和channel之间不能直接传递消息
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+}
+
+/*
+chan<- type 只接收channel
+<-chan type 只发送channel
+*/
