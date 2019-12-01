25fa21fa8c93f9e1e2a4a0508d5e54a58b39ab1f
mengdejian <mengdejian@windimg.com>
2019-12-01 16:16:52 +0800

    defer

--- 41_panic/main.go
+++ 41_panic/main.go
@@ -2,9 +2,15 @@ package main
 
 import "os"
 
+import "fmt"
+
 func main() {
 	// panic("a problem")
-
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("catch panic:", r)
+		}
+	}()
 	_, err := os.Create("/temp/file")
 	if err != nil {
 		panic(err)
---
+++ 42_defer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "os"
+
+import "fmt"
+
+func createFile(path string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	f.Close()
+}
+
+func main() {
+	f := createFile("./defer.txt")
+	defer closeFile(f) // defer 先进后出
+	writeFile(f)
+}
