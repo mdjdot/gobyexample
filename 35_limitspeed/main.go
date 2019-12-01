1ec2fe863b72aff03f579002a6f057c211dd7ed6
mengdejian <mengdejian@windimg.com>
2019-12-01 13:26:39 +0800

    limitspeed

---
+++ 35_limitspeed/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	requests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := time.Tick(2 * time.Second) // 2秒中获取一个值
+	for req := range requests {
+		fmt.Println("request", req, <-limiter)
+		fmt.Println("request", req, time.Now())
+	}
+
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(2 * time.Second) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		fmt.Println("request", req, <-burstyLimiter)
+		fmt.Println("request", req, time.Now())
+	}
+}
