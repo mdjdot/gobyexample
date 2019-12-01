eb55e0ef92a54acac090bbac447bf2c365afea4d
mengdejian <mengdejian@windimg.com>
2019-12-01 17:17:34 +0800

    combinefunction

---
+++ 43_combinefunction/main.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+import "strings"
+
+// Index .
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
+// Include .
+func Include(vs []string, t string) bool {
+	return Index(vs, t) >= 0
+}
+
+// Any .
+func Any(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// All .
+func All(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Filter .
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+// Map .
+func Map(vs []string, f func(string) string) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Index(strs, "nonpear"))
+
+	fmt.Println(Include(strs, "pear"))
+	fmt.Println(Include(strs, "nonpear"))
+
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+	fmt.Println(All(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
+	fmt.Println(Map(strs, strings.ToUpper))
+}
+
+/*
+有时候，直接使用内联组合操作代码会更清晰，而不是创建并调用一个帮助函数。
+*/
